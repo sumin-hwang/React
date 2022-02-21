fbcff97f12ab915681a9ae0748c02b4bd05b5606
sumin-hwang <suumiin.h@gmail.com>
2022-02-21 23:21:09 +0900

    CSS Module

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
-import SassComponent from './SassComponent';
 import React, {Component} from 'react';
+import CSSModule from './CSSModule';
 
 class App extends Component{
   render(){
     return(
       <div>
-        <SassComponent />
+        <CSSModule />
       </div>
     )
   }
