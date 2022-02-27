cc561b2eee00b104700836e3eef1a183d2c89a07
sumin-hwang <suumiin.h@gmail.com>
2022-02-27 23:05:09 +0900

    TodoList 동작구현

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -4,11 +4,59 @@ import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
 const App = () => {
+  
+  const [todos, setTodos] = React.useState([
+    {
+      id : 1,
+      text : '리액트의 기초 알아보기',
+      checked : true,
+    }, 
+    {
+      id : 2,
+      text : '컴포넌트 스타일링 하기',
+      checked : true,
+    },
+    {
+      id : 3,
+      text : '일정 관리 앱 만들어보기',
+      checked : false,
+    }
+  ]);
+
+  const nextId = React.useRef(4); //고윳값으로 사용될 id, ref사용해서 변수 담음 
+
+  const onInsert = React.useCallback(
+    text => {
+      const todo = {
+        id : nextId.current,
+        text,
+        checked : false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },[todos],
+  );
+
+  const onRemove = React.useCallback(
+    id => {
+      setTodos(todos.filter(todo => todo.id !== id));
+    },[todos],
+  );
+
+  const onToggle = React.useCallback(
+    id => {
+     setTodos(
+       todos.map(todo =>
+        todo.id === id ? {...todos, checked : !todo.checked} : todo,),
+     );
+    }, [todos],
+  );
+  
   return(
     <div>
       <TodoTemplate> 
-        <TodoInsert />
-        <TodoList />
+        <TodoInsert onInsert={onInsert}/>
+        <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle}/>
       </TodoTemplate>
     </div>
   )
--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({onInsert}) => {
+
+    const [value, setValue] = React.useState('');
+    const onChange = useCallback( e => {
+        setValue(e.target.value);
+    },[]);
+
+    const onSubmit = useCallback(
+        e => {
+            onInsert(value);
+            setValue('');
+            //submit이벤트는 브라우저에서 새로고침 발생시킴. 방지 위해서 
+            e.preventDefault();
+        },
+        [onInsert, value],
+    );
+
     return (
-        <form className="TodoInsert">
-            <input placeholder= "할 일을 입력하세요."/>
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input 
+                placeholder= "할 일을 입력하세요."
+                vlaue = {value}
+                onChange = {onChange} />
             <button type = "submit">
                 <MdAdd />
             </button>
