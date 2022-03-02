14ec4d4567ab83cf2ffc415c03889df39ade7841
sumin-hwang <suumiin.h@gmail.com>
2022-03-02 23:21:15 +0900

    성능 최적화

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,25 +3,38 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
+function createBulkTodos() {
+  const array = [];
+  for(let i = 1; i <= 2500; i++){
+    array.push({
+      id : i,
+      text : `할 일 ${i}`,
+      checked : false,
+    });
+  }
+  return array;
+}
+
+
 const App = () => {
   
-  const [todos, setTodos] = React.useState([
-    {
-      id : 1,
-      text : '리액트의 기초 알아보기',
-      checked : true,
-    }, 
-    {
-      id : 2,
-      text : '컴포넌트 스타일링 하기',
-      checked : true,
-    },
-    {
-      id : 3,
-      text : '일정 관리 앱 만들어보기',
-      checked : false,
-    }
-  ]);
+  const [todos, setTodos] = React.useState(createBulkTodos);
+  //   {
+  //     id : 1,
+  //     text : '리액트의 기초 알아보기',
+  //     checked : true,
+  //   }, 
+  //   {
+  //     id : 2,
+  //     text : '컴포넌트 스타일링 하기',
+  //     checked : true,
+  //   },
+  //   {
+  //     id : 3,
+  //     text : '일정 관리 앱 만들어보기',
+  //     checked : false,
+  //   }
+  // ]);
 
   const nextId = React.useRef(4); //고윳값으로 사용될 id, ref사용해서 변수 담음 
 
@@ -47,7 +60,7 @@ const App = () => {
     id => {
      setTodos(
        todos.map(todo =>
-        todo.id === id ? {...todos, checked : !todo.checked} : todo,),
+        todo.id === id ? {...todo, checked : !todo.checked} : todo,),
      );
     }, [todos],
   );
