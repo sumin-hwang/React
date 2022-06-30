8d8f230377099af949e6c86c57c0c6bc924d55d5
sumin-hwang <suumiin.h@gmail.com>
2022-07-01 00:17:06 +0900

    Route로 특정 주소에 컴포넌트 연결

--- router-tutorial/src/index.js
+++ router-tutorial/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-React.DOM.render(
+ReactDOM.render(
   <BrowserRouter>
-    <App/>
+    <App/ >
   </BrowserRouter>,
   document.getElementById('root')
-
 );
