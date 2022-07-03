396b9a2ee008717f78ff9490e83d41b9cc056003
sumin-hwang <suumiin.h@gmail.com>
2022-07-03 23:05:12 +0900

    use Link Component

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import {Route, Routes} from 'react-router-dom';
+import {Route, Routes, Link} from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 
 const App = () => {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/"> HOME </Link>
+        </li>
+        <li>
+          <Link to="/about"> About</Link>
+        </li>
+      </ul>
+      <hr/>
       <Routes>
         <Route path ="/" element={<Home />} exact={true}/> {/**<Route>사용시 <Routes>로 묶어주어야 하며, component = {HOME}대신 element ={<Home />}처럼 사용 */}
         <Route path ="/about" element={<About />} />
       </Routes>
-      <h1>
-        test
-      </h1>
-    </div>
+    </div>     
   );
 };
 
