2da95b01e8fe5d113e1ad9b80e00a893ce052e87
sumin-hwang <suumiin.h@gmail.com>
2022-07-13 00:09:55 +0900

    useNavigate

--- router-tutorial/src/About.js
+++ router-tutorial/src/About.js
@@ -1,32 +1,15 @@
 import React from 'react';
 import { useLocation, useSearchParams } from 'react-router-dom';
-
 const About = () => {
-    const [searchParams, setSearchParams] = useSearchParams(); //쿼리스트링 파싱 
-    const detail = searchParams.get('detail');
-    const mode = searchParams.get('mode');
     const location = useLocation();
 
-    const onToggleDetail = () => {
-        setSearchParams({mode, detail : detail === 'true' ? false : true});
-    }
-
-    const onIncreaseMode = () => {
-        const nextMode = mode === null ? 1 : parseInt(mode) + 1;
-        setSearchParams({mode : nextMode, detail});
-    }
-
     return (
-        <div>
-            <h1> Introduction </h1>
-            <p> This Project is react router sample page</p>
-            <p> Query String : {location.search}</p>
-            <p> detail : {detail}</p>
-            <p> mode : {mode} </p>
-            <button onclick={onToggleDetail}> Toggle detail</button>
-            <button onClick={onIncreaseMode}> mode + 1</button>
-        </div>
+      <div>
+        <h1>소개</h1>
+        <p>리액트 라우터를 사용해 보는 프로젝트입니다.</p>
+        <p>쿼리스트링: {location.search}</p>
+      </div>
     );
-};
-
-export default About;
+  };
+  
+  export default About;
