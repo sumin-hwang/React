046b8c9ade3966fbf8dad8c2a612b447266e99e8
sumin-hwang <suumiin.h@gmail.com>
2022-07-15 00:19:40 +0900

    Navigate, Redirect

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -5,6 +5,9 @@ import Articles from './Articles';
 import Home from './Home';
 import Profile from './Profile';
 import Layout from './Layout';
+import NotFound from './NotFound';
+import Login from './Login';
+import MyPage from './MyPage';
 
 const App = () => {
   return (
@@ -17,6 +20,9 @@ const App = () => {
       <Route path="/articles" element={<Articles />} >
         <Route path="/articles/:id" element={<Article />} /> {/**중첩된 라우트는 Outlet 태그 사용으로 볼 수 있음 */}
       </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/mypage" element={<MyPage />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
