77a2fea854a61bbb3a8c06ae0623a559dc49b93a
sumin-hwang <suumiin.h@gmail.com>
2023-07-10 23:14:51 +0900

    news viewer ui만들기

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,27 +1,9 @@
 import React, {useState} from 'react';
 import axios from 'axios';
+import NewsList from './components/NewsList'
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async() => {
-    try {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=9691c521d2ce4ab393fec3eb9b850851'
-      );
-      setData(response.data);
-    }catch(e){
-      console.log(e);
-    }
-  };
-
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-    </div>
-  );
+  return <NewsList />;
 };
 
 export default App;
