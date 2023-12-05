49d40114f45efca38bbded9b79249a02e9627346
sumin-hwang <suumiin.h@gmail.com>
2023-12-05 21:43:05 +0900

    code splitting 예제

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,17 +1,34 @@
+import React, {useState, Suspense} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import notify from './notify';
+// const SplitMe = React.lazy(() => import('./SplitMe'));
+// import notify from './notify';
+import loadable from '@loadable/copmponent';
+const SplitMe = loadable(() => import('./SplitMe'), {
+  fallback : <div>loading...</div>
+});
+
 
 function App() {
+  const [visible, setVisible] = useState(false);
+
   const onClick = () => {
-    notify();
+    // notify();
+    // import ('./notify').then(result => result.default());
+    setVisible(true);
   }
+
+  
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className ="App-logo" alt="logo " />
         <p onClick={onClick}> Hello React!</p>
+        {/* <Suspense fallback={<div>loading...</div>}>
+          {visible && <SplitMe/>}
+        </Suspense> */}
+        {visible && <SplitMe />}
       </header>
     </div>
   );
